69e26c420ca53af4c11e1b809bfd6c76ef814b94
Paul McLaughlin <pmclaughlin@navinet.net>
2013-01-28 17:28:48 +0000

    Interpreter pattern added

---
+++ Design Practice/Interpreter/Contexts/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interpreter.Contexts
+{
+    class Context
+    {
+        private string _input;
+        private int _output;
+
+        //constructor
+        public Context(string input)
+        {
+            _input = input;
+        }
+
+        public string Input
+        {
+            get { return _input; }
+            set { _input = value; }
+        }
+
+        public int Output
+        {
+            get { return _output; }
+            set { _output = value; }
+        }
+    }
+}
---
+++ Design Practice/Interpreter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interpreter.Contexts;
+using Interpreter.Expressions;
+using Interpreter.TerminalExpressions;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string roman = "MMCMXXVIII";
+            Context context = new Context(roman);
+
+            //Build the 'parse tree'
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+
+            //Interpret
+
+            foreach (Expression expression in tree)
+            {
+                expression.Interpret(context);
+            }
+
+            Console.WriteLine("{0} = {1}",roman, context.Output);
+
+            //wait for user input
+            Console.ReadKey(true);
+        }
+    }
+}
