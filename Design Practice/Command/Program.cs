d426faeb22a1db6087f78b3ebaebf6ad8d96c60b
Paul McLaughlin <pmclaughlin@navinet.net>
2013-01-28 16:52:30 +0000

    Added in command pattern

---
+++ Design Practice/Command/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Command.Invoker;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create user and let her compute
+            User user = new User();
+
+            //user presses calculator buttons
+            user.Compute('+',100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+
+            //undo all 4 commands
+            user.Undo(4);
+
+            //redo 3 commands
+            user.Redo(3);
+
+            //wait for user
+            Console.ReadKey(true);
+
+        }
+    }
+}
