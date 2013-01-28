1d6aaadb56d4fdc95b0e9735e96ed6985f9ad2c4
Paul McLaughlin <pmclaughlin@navinet.net>
2013-01-28 16:00:58 +0000

    Added in the proxy pattern

---
+++ Design Practice/ProxyPattern/Subjects/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyPattern.Subjects
+{
+    class Math :IMath
+    {
+        public double Add(double x, double y)
+        {
+            return x + y; 
+        }
+
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
