64134e7ddc5228383344196592f489d3a475fdb0
Paul McLaughlin <pmclaughlin@navinet.net>
2013-01-28 14:18:36 +0000

    Added Adapter pattern

---
+++ Design Practice/Adapter/Target/Compound.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter.Target
+{
+    public class Compound
+    {
+        protected string _chemical;
+        protected float _boilingPoint;
+        protected float _meltingPoint;
+        protected double _molecularWeight;
+        protected string _molecularFormula;
+
+        //constructor
+        public  Compound (string chemical)
+        {
+            _chemical = chemical;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine(string.Format("\nCompound : {0} ----- ",_chemical));
+        }
+    }
+}
